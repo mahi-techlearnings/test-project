62d58777db90ea374d34a1c6cdc54aceab9e550a
Mahesh Kumawat <mahesh19nov@gmail.com>
2024-05-02 17:02:06 +0530

    Initial hello world program

---
+++ src/main/java/com/example/testproject/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.example.testproject.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+    @GetMapping
+    public ResponseEntity<?> sayHello() {
+        return ResponseEntity.ok("Hello World!");
+    }
+}
